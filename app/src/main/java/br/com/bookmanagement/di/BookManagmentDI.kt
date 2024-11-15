6ff38ad994b4760c45bc094dd55ec72cc89bb533
Danilo Ferreira <dancostasf@gmail.com>
2024-11-15 15:59:43 -0300

    add viewmodel e injecao

--- app/src/main/java/br/com/bookmanagement/di/BookManagmentDI.kt
+++ app/src/main/java/br/com/bookmanagement/di/BookManagmentDI.kt
@@ -1,11 +1,19 @@
 package br.com.bookmanagement.di
 
+import br.com.bookmanagement.data.repository.BookRepository
+import br.com.bookmanagement.data.repository.IBookRepository
+import br.com.bookmanagement.domain.usecase.ReaderManualUseCase
 import org.koin.dsl.module
 
 object BookManagmentDI {
     val bookModule = module {
+        factory<IBookRepository> {
+            BookRepository(
+                get()
+            )
+        }
         factory {
-
+            ReaderManualUseCase(get())
         }
     }
 }
--- app/src/main/java/br/com/bookmanagement/presentation/MainActivity.kt
+++ app/src/main/java/br/com/bookmanagement/presentation/MainActivity.kt
@@ -13,13 +13,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import br.com.bookmanagement.di.BookManagmentDI
 import br.com.bookmanagement.presentation.navigation.NavigationGraph
 import br.com.bookmanagement.ui.theme.BookManagementTheme
+import org.koin.core.context.loadKoinModules
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        loadKoinModules(BookManagmentDI.bookModule)
+
         setContent {
             BookManagementTheme {
                 val navController = rememberNavController()
